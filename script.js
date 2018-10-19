9776d2b4accff053cdb288f9a5c68e7fa180be86
m4nG0sM00th13 <manuelgonzalezcashun@ccpaedu.com>
2018-10-19 01:29:12 +0000

    ubisoft lol

---
+++ script.js
@@ -0,0 +1,15 @@
+var userChoice;
+var computerChoice;
+var winner;
+
+var computerChoice = Math.Random();
+console.log(computerChoice);
+
+$("#shoot").click(function(){
+  var user = $("#input").val();
+  $("#userChoice").text(user);
+});
+if(userChoice = rock)
+{
+  
+}
