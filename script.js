ea4f9b331d2ab1c03af3396952831c99beb97c70
m4nG0sM00th13 <manuelgonzalezcashun@ccpaedu.com>
2018-10-26 01:30:10 +0000

    Computer Choice is displayed

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 var userChoice;
 var computerChoice;
 var winner;
-
-var computerChoice = Math.Random();
-console.log(computerChoice);
+var randomNumber = 0;
 
 $("#shoot").click(function(){
-  var user = $("#input").val();
+  var user = $("input").val();
   $("#userChoice").text(user);
-});
-if(userChoice = rock)
-{
   
+  var computerChoice = Math.random();
+ 
+  
+  if(computerChoice > 0.0 && computerChoice < 0.30){
+   $("#computerChoice").text("rock");
+   computerChoice = "rock";
 }
+else if(computerChoice > 0.30 && computerChoice < 0.60){
+   $("#computerChoice").text("paper");
+   computerChoice = "paper";
+}
+else{
+   $("#computerChoice").text("scissors");
+   computerChoice = "scissors";
+}
+if(userChoice === "rock" && computerChoice === "paper"){
+  $("#result").text("Computer wins")
+}
+});
+
